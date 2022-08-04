f7bbfee75ae26b557263b6a072b52d25d81472b2
Dario Cancelliere <dario.cancelliere@prima.it>
2022-08-04 19:08:13 +0200

    Little improvement

--- 70-ClimbingStairs.cpp
+++ 70-ClimbingStairs.cpp
@@ -6,11 +6,13 @@ int ClimbStairs::climbStairs(int n) {
     for (int i = n; i > 0; i--) {
         if (i == n) {
             oneStep = 1;
-        } else {
-            int oldStep = oneStep;
-            oneStep = oneStep + twoStep;
-            twoStep = oldStep;
+
+            continue;
         }
+
+        int oldStep = oneStep;
+        oneStep = oneStep + twoStep;
+        twoStep = oldStep;
     }
 
     return oneStep;
