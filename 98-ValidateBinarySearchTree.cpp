e7a2ae93a2e883e3406cc97f2f463a82b9f1eae0
Dario Cancelliere <dario.cancelliere@gmail.com>
2022-08-07 23:39:55 +0200

    Added "98. Validate Binary Search Tree" exercise

---
+++ 98-ValidateBinarySearchTree.cpp
@@ -0,0 +1,51 @@
+#include "98-ValidateBinarySearchTree.h"
+
+#include <stack>
+
+using namespace std;
+
+bool ValidateBinarySearchTree::validateBST(TreeNode *root, long long int max, long long int min) {
+    if (root == nullptr) {
+        return true;
+    }
+
+    if (root->val >= max || root->val <= min) {
+        return false;
+    }
+
+    return validateBST(root->left, root->val, min) && validateBST(root->right, max, root->val);
+}
+
+bool ValidateBinarySearchTree::isValidBST(TreeNode *root) {
+    return validateBST(root);
+}
+
+bool ValidateBinarySearchTree::isValidBSTIterative(TreeNode *root) {
+    if (root == nullptr) {
+        return true;
+    }
+
+    stack<TreeNode *> st;
+    TreeNode *cursor = root, *previous = nullptr;
+
+    while (cursor != nullptr || !st.empty()) {
+        if (cursor != nullptr) {
+            st.push(cursor);
+            cursor = cursor->left;
+
+            continue;
+        }
+
+        cursor = st.top();
+        st.pop();
+
+        if (previous != nullptr && cursor->val <= previous->val) {
+            return false;
+        }
+
+        previous = cursor;
+        cursor = cursor->right;
+    }
+
+    return true;
+}
---
+++ 98-ValidateBinarySearchTree.h
@@ -0,0 +1,23 @@
+#ifndef LEETCODE_PROBLEMS_98_VALIDATEBINARYSEARCHTREE_H
+#define LEETCODE_PROBLEMS_98_VALIDATEBINARYSEARCHTREE_H
+
+
+#include <climits>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+class ValidateBinarySearchTree {
+private:
+    static bool validateBST(TreeNode *root, long long int max = LLONG_MAX, long long int min = LLONG_MIN);
+
+public:
+    static bool isValidBST(TreeNode *root);
+    static bool isValidBSTIterative(TreeNode *root);
+};
+
+
+#endif //LEETCODE_PROBLEMS_98_VALIDATEBINARYSEARCHTREE_H
